8d789a2f213c262beed39ed43f36378b4fe5d7d7
kimbeff <kimbeff@gmail.com>
2025-03-15 20:23:39 +0200

    w

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Router>
+      <Analytics />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
